36f874df978b0a6bc6d00bacaf27ad532e8d105c
phlames <kelechiikeolu@gmail.com>
2024-12-04 10:50:20 +0100

    completed logic for Add btn

---
+++ app.js
@@ -0,0 +1,29 @@
+// CALLING THE GLOBAL VARIABLES
+
+const inputOne = document.getElementById("inputOne");
+const inputTwo = document.getElementById("inputTwo");
+const addBtn = document.getElementById("addBtn");
+const subBtn = document.getElementById("subBtn");
+const displaydiv = document.getElementById("displaydiv");
+
+// CREATING A RESET FUNCTION
+
+function reset() {
+  inputOne.value = "";
+  inputTwo.value = "";
+}
+
+// ADD A CLICK EVENT LISTENER ON THE ADD BUTTON
+
+addBtn.addEventListener("click", () => {
+  const num1 = parseFloat(inputOne.value);
+  const num2 = parseFloat(inputTwo.value);
+
+  if (isNaN(num1) && isNaN(num2)) {
+    displaydiv.textContent = "please input a valid number";
+  } else {
+    result = num1 + num2;
+    displaydiv.textContent = `Result: ${result}`;
+  }
+  reset();
+});
